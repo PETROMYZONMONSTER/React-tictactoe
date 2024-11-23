65cb67079ef39004179b41f0ed331a9e1d6cc367
petromyzontiform <petromyzonmonster@gmail.com>
2024-11-23 17:12:28 -0300

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function TicTacToe() {
+  const [turn, setTurn] = useState("X");
+  const [cells, setCells] = useState(Array(9).fill("·"));
+  const [winner, setWinner] = useState();
+  const [isDraw, setIsDraw] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const checkWinner = (arr) => {
+    let combos = {
+      across: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      ],
+      down: [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+      ],
+      diagonal: [
+        [0, 4, 8],
+        [2, 4, 6],
+      ],
+    };
+    for (let combo in combos) {
+      combos[combo].forEach((pattern) => {
+        if (
+            arr[pattern[0]] === "·" ||
+            arr[pattern[1]] === "·" ||
+            arr[pattern[2]] === "·"
+        ) {
+        } else if (
+            arr[pattern[0]] === arr[pattern[1]] &&
+            arr[pattern[1]] === arr[pattern[2]]
+        ) {
+          setWinner(arr[pattern[0]]);
+        }
+      });
+    }
+  };
+
+  const handleClick = (num) => {
+    if (winner || cells[num] !== "·") return;
+
+    let arr = [...cells];
+    if (turn === "X") {
+      arr[num] = "X";
+      setTurn("O");
+    } else {
+      arr[num] = "O";
+      setTurn("X");
+    }
+    checkWinner(arr);
+    setCells(arr);
+    if (!arr.includes("·") && !winner) {
+      setIsDraw(true);
+    }
+  };
+
+  const Cell = ({ num }) => {
+    const cellValue = cells[num];
+    const cellClassName = cellValue ? `cell cell-${cellValue}` : "cell";
+
+    return (
+        <td className={cellClassName} onClick={() => handleClick(num)}>
+          {cellValue}
+        </td>
+    );
+  };
+
+  const handleReset = () => {
+    setWinner(null);
+    setIsDraw(false);
+    setCells(Array(9).fill("·"));
+  };
+
+  document.body.classList.add("light-theme");
+
+  const toggleTheme = () => {
+    let element = document.body;
+    element.classList.toggle("dark-theme")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="general_container">
+        <div className="game_container">
+          <div className="division_container">
+            <div className="player_container">
+              <h3>Player 1</h3>
+              <div className={`winner ${winner || isDraw ? "show" : null}`}>
+                {winner ? `${winner} wins!` : isDraw ? "It's a draw!" : null}
+              </div>
+            </div>
+          </div>
+          <div className="division_container">
+            <h1>React TicTacToe </h1>
+            <table>
+              <tbody>
+              <tr>
+                <Cell num={0}/>
+                <Cell num={1}/>
+                <Cell num={2}/>
+              </tr>
+              <tr>
+                <Cell num={3}/>
+                <Cell num={4}/>
+                <Cell num={5}/>
+              </tr>
+              <tr>
+                <Cell num={6}/>
+                <Cell num={7}/>
+                <Cell num={8}/>
+              </tr>
+              </tbody>
+            </table>
+            <button className="basic_button" onClick={toggleTheme}>
+              Theme
+            </button>
+          </div>
+          <div className="player_container">
+            <h3>Player 2</h3>
+            <div className={`winner ${winner || isDraw ? "show" : null}`}>
+              {winner ? `${winner} wins!` : isDraw ? "It's a draw!" : null}
+            </div>
+          </div>
+        </div>
+        <button className="basic_button" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
   );
 }
 
-export default App;
